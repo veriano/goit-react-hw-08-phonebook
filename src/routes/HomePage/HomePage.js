0a875e9d74b0b0c1a780a7a83c4bbb14c18a949d
valeriy <veriano@ukr.net>
2022-04-03 13:29:41 +0300

    finishing

--- src/routes/HomePage/HomePage.js
+++ src/routes/HomePage/HomePage.js
@@ -1,12 +1,35 @@
 import React from "react";
+import { motion, AnimatePresence } from 'framer-motion';
 import s from './HomePage.module.css';
+import { variants } from 'utils/motion';
 
 function HomePage() {
      return (
           <>
-               <h1 className={s.home}>Добро пожаловать в приложение для создания<br />книги контактов!</h1>
-               <h2 className={s.home}>Для получения доступа к вашей книге контактов пожалуйста зарегистрируйтесь или выполните вход.</h2>
-         </>
+               <AnimatePresence>
+                    <motion.h1 className={s.home}
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    transition="transition"
+                    variants={variants}
+                    >
+                    Добро пожаловать в приложение для создания<br />книги контактов!
+                    </motion.h1>
+               </AnimatePresence>
+               <AnimatePresence>
+                    <motion.h2 className={s.home}
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    transition="transition"
+                    variants={variants}
+                    >
+                    Для получения доступа к вашей книге контактов пожалуйста зарегистрируйтесь или выполните вход.
+                    </motion.h2>
+               </AnimatePresence>
+               </>
      )
+          
 }
 export default HomePage;
---
+++ src/utils/motion.js
@@ -0,0 +1,6 @@
+export const variants = {
+  initial: { scale: 0.2 },
+  animate: { scale: 1 },
+  exit: { scale: 0 },
+  transition: { ease: 'easeInOut', duration: 0.3 },
+};
